ea65f1ab4cc63ad7b30b563b01ad2f17ee2c4d9d
Kadi Laidoja <kadi.laidoja@gmail.com>
2015-03-05 17:36:33 +0100

    jälle pisiasjad

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// function testFun() {
+// 	document.getElementById("pealkiri").innerHTML = "testime JS-i";
+// }
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("#pealkiri").fadeOut("slow");
+//         $("img").fadeIn("slow");
+//     });
+// });
+
+// $(document).ready(function(){
+// $("input").focus(function(){
+//     $(this).css("background-color", "#cccccc");
+// });
+// });
+
+// $(document).ready(function(){
+// $("input").blur(function(){
+//     $(this).css("background-color", "#ffffff");
+// });
+// });
+
