a87afaccb3226c52567f6f2084a7241357664692
Kadi Laidoja <kadi.laidoja@gmail.com>
2015-03-08 12:43:03 +0100

    dropdownist saab valida, joondus

--- js/script.js
+++ js/script.js
@@ -21,15 +21,12 @@
 // });
 // });
 
-   $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
-
-      var $target = $( event.currentTarget );
-
-      $target.closest( '.btn-group' )
-         .find( '[data-bind="label"]' ).text( $target.text() )
-            .end()
-         .children( '.dropdown-toggle' ).dropdown( 'toggle' );
-
-      return false;
-
+$(document).ready(function(){
+   $('body').on('click','.option li',function(){
+	var i = $(this).parents('.select').attr('id');
+	var v = $(this).children().text();
+	var o = $(this).attr('id');
+	$('#'+i+' .selected').attr('id',o);
+	$('#'+i+' .selected').text(v);
+});
    });
