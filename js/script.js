0a24acef99a07211ca80683d5cbb074c3d133415
Kadi Laidoja <kadi.laidoja@gmail.com>
2015-03-12 12:24:42 +0100

    pilt vahetub

--- js/script.js
+++ js/script.js
@@ -22,11 +22,36 @@
 // });
 
 $(document).ready(function(){
-   $('body').on('click','.option li',function(){
-	var i = $(this).parents('.select').attr('id');
-	var v = $(this).children().text();
-	var o = $(this).attr('id');
-	$('#'+i+' .selected').attr('id',o);
-	$('#'+i+' .selected').text(v);
+	$('body').on('click','.option li',function(){
+		var i = $(this).parents('.select').attr('id');
+		var v = $(this).children().text();
+		var o = $(this).attr('id');
+		$('#'+i+' .selected').attr('id',o);
+		$('#'+i+' .selected').text(v);
+	});
 });
-   });
+
+// $("#seade-dropdown li a").selected(function(){
+//   var selText = $(this).text();
+//   alert("tkktk");
+//   $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+// });
+
+// $(document).ready(function(){
+// 	$("#seade-dropdown li a").click(function(){
+// 		//alert("hei");
+// 		alert($("#test").html());
+// 	});
+// });
+$(document).ready(function(){
+	$('#seade').change(function(){
+		$('#fireplace').src = this.value;
+		$('#fireplace').attr('src', $(this).val());
+	});
+});
+
+// $(function(){
+// 	$('#theSelect').on('change', function(){
+// 		$('#theImage').attr('src', $(this).val() + '.png');
+// 	});
+// });
